b0069c06d616f923c34b12b768cbf4c4e6ffec4d
Divyam Nandanwar <Divyam.Nandanwar@igglobal.com>
2019-05-16 18:07:29 +0530

    Initial streaming code

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const path = require('path');
 const _ = require('lodash');
 const config = JSON.parse(fs.readFileSync('./config.json'));
 
 app.get('/', (req, res) => {
     res.send("hello");
+});
+
+app.get('/track', (req, res) => {
+    const relPath = _.get(config, "RELATIVE_PATH", ["..", "audio"]);
+    const filePath = path.join(__dirname, ...relPath, "aot.mp3");
+    const stream = fs.createReadStream(filePath);
+
+    console.log("Sending track");
+    res.set('content-type', 'audio/mp3');
+    res.set('accept-ranges', 'bytes');
+    stream.on("data", chunk => {
+        res.write(chunk);
+    });
+
+    stream.on("err", () => {
+        res.sendStatus(404);
+    });
+
+    stream.on("end", () => {
+        res.end();
+        console.log("Ending track");
+    });
 });
 
 const port = _.get(config, "PORT", 8080);
